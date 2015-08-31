7c6189964e818d058c394225510d3258a695c4a0
evalle <shmarnev@gmail.com>
2015-08-31 17:37:59 +0200

    Put some shcnges in main file

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+# global imports;
 import forecastio
 import datetime
-import location
 
+# local import:
+import location
+import bcolors
+
+# my api_key (you can register yours here: https://developer.forecast.io/ it's free!
 api_key = "4a879c08204fde4826098bd13d0c66b3"
 
+# some variables 
 lat = location.geoip.latitude
 lng = location.geoip.longitude
 city = location.geoip.city
+# colors:
+blue = bcolors.Colors.BLUE
+green = bcolors.Colors.GREEN
+yellow = bcolors.Colors.YELLOW
+red = bcolors.Colors.RED
+end = bcolors.Colors.END
 
 current_time = datetime.datetime.now()
 
@@ -17,12 +29,12 @@ byHour = forecast.hourly()
 def output(city):
     
     if city != None:
-        print "You're in %s now " % city
+        print "You're in %s now " % (green + city + end)
     else:
         print "Currently we can't find in which city you're now"
     
-    print "Forecast for a next hour is:"
-    print byHour.summary
+    print "Forecast is:"
+    print green + byHour.summary + end
 #   print byHour.icon
 
 output(city)
