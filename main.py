4fc3dace61b1e5c679c1fd6021d305dcf4680941
evalle <shmarnev@gmail.com>
2015-09-09 15:07:51 +0200

    Small refactoring
    Signed-off-by: evalle <shmarnev@gmail.com>

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 import lizepy
 import forecastio
 import datetime
+import argparse
 
 # local imports:
 import bcolors
@@ -51,6 +52,15 @@ rawcloudcover = byNow.cloudCover
 rawpressure = int(byNow.pressure * 0.7500637554192) # mbar to mmHg
 weathersum = byNow.summary
 
+# argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('--location', '-l',
+        help = 'your location')
+
+args = parser.parse_args()
+city_user = args.location
+#print(args.location)
+
 # functions
 def current_temp(rawtemp):
 
@@ -84,17 +94,7 @@ def color_cloudcover(rawcloudcover):
         cloudcover = (yellow + str(rawcloudcover) + end)
     return cloudcover
 
-# main function
-def output(city_ip):
-
-    print()
-    print("Current weather is " + green + weathersum + end, fancy_icon(weathersum))
-    
-    if city_ip != None:
-        print("in %s, %s " % (red + city_ip, country_ip + end))
-    else:
-        print("in %s, %s " % (red + city_google, country_google + end))  
-
+def print_info():
     print()
     print("The temperature is %s°C, but it feels like %s°C" % ((current_temp(rawtemp)), (current_temp(rawfeelsliketemp))))
     print("The windspeed is %s m/s" % (yellow + windspeed + end))
@@ -102,4 +102,16 @@ def output(city_ip):
     print("The pressure is %s mmHg" % (yellow + str(rawpressure) + end))
     print()
 
-output(city_ip)
+# main function
+def output(city_ip, city_user):
+
+    print()
+    print("Current weather is " + green + weathersum + end, fancy_icon(weathersum))
+    
+    if city_user == None:
+        if city_ip != None:
+            print("in %s, %s " % (red + city_ip, country_ip + end))
+        else:
+            print("in %s, %s " % (red + city_google, country_google + end))  
+        
+output(city_ip, city_user)
