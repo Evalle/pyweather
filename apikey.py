b28a84466cbcf764929c826fd30850f1cf544d12
evalle <shmarnev@gmail.com>
2015-10-14 18:23:50 +0200

    Change pyweather

--- apikey.py
+++
@@ -1,1 +0,0 @@
-key = '' # your API key here
--- bcolors.py
+++
@@ -1,9 +0,0 @@
-# add some basic colors
-
-class Colors:
-
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    END = '\033[0m'
