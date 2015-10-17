f60bbf001b5d7414c36a0b35658697fae3ac7d43
evalle <shmarnev@gmail.com>
2015-10-17 18:51:17 +0200

    write some test code for cpkey script

--- cpkey.py
+++ cpkey.py
@@ -1,1 +1,7 @@
-# create apikey.py file and copy api key to it
+# This scrpit creates apikey.py file and copies your api key to this file. 
+
+print "Please, paste your api key file"
+
+user_input = raw_input("> ")
+
+print user_input
