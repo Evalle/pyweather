416f1a46e6fb4aa30dee7394afb5cc6f6ebf98ac
evalle <shmarnev@gmail.com>
2015-10-18 13:05:44 +0200

    add colors and compatibility with Python3

--- cpkey.py
+++ cpkey.py
@@ -1,5 +1,17 @@
 # This scrpit creates apikey.py file and copies your api key to this file. 
 
+# add some basic colors
+
+from __future__ import print_function
+
+class Colors:
+
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    END = '\033[0m'
+
 print "Please, paste your api key file"
 
 user_input = raw_input("> ")
