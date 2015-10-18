8674941a13a07ae39ec024e821c4fa16eebefed6
evalle <shmarnev@gmail.com>
2015-10-18 13:43:23 +0200

    remove some colors from cpkey.py script

--- cpkey.py
+++ cpkey.py
@@ -8,9 +8,9 @@ class Colors:
     RED = '\033[91m'
     END = '\033[0m'
 
-print(Colors.RED + """
+print("""
 This script is going to erase apikey.py file please press 'Ctrl+C'if you don't want to do it and a 'Enter' if you do want it
-""" + Colors.END)
+""")
 
 raw_input("?")
 print("")
